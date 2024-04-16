7efa18cf97c60fc2f0a342864bb780c2faf6026f
LinusArdell <linus2016chaesa@mhs.mdp.ac.id>
2024-04-16 10:12:53 +0700

    Check connection access

--- app/src/main/java/com/test/input/Activity/Splash.java
+++ app/src/main/java/com/test/input/Activity/Splash.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.test.input.R;
 
@@ -32,10 +36,30 @@ public class Splash extends AppCompatActivity {
         HDL.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(Splash.this, LoginActivity.class));
-                finish();
+                if (isNetworkStatusAvialable(getApplicationContext())){
+                    startActivity(new Intent(Splash.this, LoginActivity.class));
+                    finish();
+                } else {
+                    HDL.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(Splash.this, "Tidak ada akses internet", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+
+                    },1000);
+                }
             }
         }, 2000);
+    }
+
+    public static boolean isNetworkStatusAvialable (Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
 
     }
 }
